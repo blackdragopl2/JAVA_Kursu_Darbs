4b8ac0c866ab97ebfa80ba4ce6b734e6cdd64f66
blackdragopl2 <thisishororo@gmail.com>
2022-06-19 15:20:39 +0300

    Created new project

---
+++ src/main/java/com/kurss/demo/KursuDarbsApplication.java
@@ -0,0 +1,13 @@
+package com.kurss.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursuDarbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KursuDarbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kurss/demo/KursuDarbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurss.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursuDarbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
